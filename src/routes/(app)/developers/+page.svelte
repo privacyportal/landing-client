b993ca73a2842803f0609d6b3efdef9fe604f56e
privacyguard <privacyguard@users.noreply.github.com>
2024-05-21 00:29:28 +0300

    add canonical links for duplicate pages

--- src/routes/(app)/developers/+page.svelte
+++ src/routes/(app)/developers/+page.svelte
@@ -1,8 +1,18 @@
 <script>
   import { goto } from '$app/navigation';
+  import FlexContainer from '$lib/components/common/FlexContainer.svelte';
+  import { DOMAIN } from '$lib/modules/constants';
   import { onMount } from 'svelte';
 
   onMount(() => {
     goto('/developers/api-docs', { replaceState: true });
   });
 </script>
+
+<svelte:head>
+  <link rel="canonical" href={`https://${DOMAIN}/developers`} />
+</svelte:head>
+
+<FlexContainer column height="calc(100vh - 50px)" align_items="center" justify_content="center" color="var(--text-color)">
+  <h3 class="no-margin">Loading...</h3>
+</FlexContainer>
