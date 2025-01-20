ddb93ec3b547ed5e939a9ea61236d99dcaf5c7a9
privacyguard <privacyguard@users.noreply.github.com>
2025-01-21 00:30:29 +0200

    using setTimeout instead of setImmediate for backend compatibility

--- src/routes/(app)/federated/rss/inbox/+server.js
+++ src/routes/(app)/federated/rss/inbox/+server.js
@@ -99,14 +99,14 @@ export async function _processInboxMessage({ message, headers, accountObj }) {
 
   // send accept message
   const acceptMessage = createAcceptMessage({ messageBody: body, actor: accountObj.PROFILE });
-  setImmediate(async () => {
+  setTimeout(async () => {
     await signAndSendMessage({
       message: acceptMessage,
       inbox,
       actor: accountObj.PROFILE,
       privkey: accountObj.TYPE === 'Group' ? env.ACTIVITYPUB_GROUP_PRIVKEY : env.ACTIVITYPUB_USER_PRIVKEY
     }).catch(console.error);
-  });
+  }, 1000);
 
   return new Response(JSON.stringify({ message: 'Data received successfully' }), {
     status: 200,
