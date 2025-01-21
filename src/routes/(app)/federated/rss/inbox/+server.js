99407446360a21d2f331a96f8b3966fdd8711d7d
privacyguard <privacyguard@users.noreply.github.com>
2025-01-21 13:02:49 +0200

    fix signatures

--- src/routes/(app)/federated/rss/inbox/+server.js
+++ src/routes/(app)/federated/rss/inbox/+server.js
@@ -104,7 +104,7 @@ export async function _processInboxMessage({ message, headers, accountObj }) {
     inbox,
     keyInfo: {
       // we always use the user key since it's also the moderator of the group
-      id: ACTIVITYPUB_ACCOUNT.PROFILE,
+      id: ACTIVITYPUB_ACCOUNT.KEY_ID,
       private: env.ACTIVITYPUB_USER_PRIVKEY
     }
   });
