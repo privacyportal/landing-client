a9b6391b3a114fe3160cf613349b6b547473e30b
privacyguard <privacyguard@users.noreply.github.com>
2025-01-21 19:31:14 +0200

    add publishing route for AP blogger account

---
+++ src/routes/(app)/federated/publisher/publish/+server.js
@@ -0,0 +1,32 @@
+import { authorize } from '$lib/modules/auth';
+import { APUB_BLOG_ACCOUNT, UNAUTHORIZED_ERR } from '$lib/modules/constants';
+import { error, isHttpError } from '@sveltejs/kit';
+import { _processPublishRequest } from '../../rss/publish/+server';
+
+export const prerender = false;
+
+// we should not publish more than 5 posts in order to not spam servers
+// this shouldn't happen anyway
+const MAX_ITEMS = 5;
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ request, url, fetch }) {
+  try {
+    if (!(request.headers.get('Accept') || '').includes('application/json')) {
+      return error(404, 'Page not found.');
+    }
+    if (!(await authorize(request).catch(() => null))) return error(401, UNAUTHORIZED_ERR);
+
+    const lastPublished = url.searchParams.get('last');
+    if (!lastPublished) return error(403, '"last" param required.');
+
+    const outbox = await fetch(APUB_BLOG_ACCOUNT.OUTBOX_PATH)
+      .then((res) => res.json())
+      .then((data) => data.orderedItems.slice(0, MAX_ITEMS));
+    return await _processPublishRequest(APUB_BLOG_ACCOUNT, outbox, lastPublished);
+  } catch (err) {
+    if (isHttpError(err)) throw err;
+    console.error(err);
+    error(400, 'Unexpected error.');
+  }
+}
